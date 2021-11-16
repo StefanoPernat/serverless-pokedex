f5f88e7d1531e6ff1369f034378bc6d272b285d4
Stefano Pernat <stefano.pernat@trentinosviluppo.it>
2021-11-16 17:50:25 +0100

    feat: basic navbar

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
   <div>
+    <Navbar />
     <router-view></router-view>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import Navbar from '@/components/Navbar.vue'
+</script>
 
 <style>
+body {
+  padding: 0;
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,58 @@
+<template>
+  <nav class="navbar">
+    <span class="logo"></span>
+    <span>Home</span>
+    <div class="search">
+      <span>Region</span>
+      <input type="text" />
+    </div>
+  </nav>
+</template>
+
+<script setup></script>
+
+<style>
+.navbar {
+  padding: 1em;
+  display: flex;
+  justify-content: space-between;
+}
+
+.navbar span:not(.logo) {
+  align-self: center;
+}
+
+.search {
+  flex-grow: 2;
+}
+
+.logo {
+  display: block;
+  background: url('@/assets/pokeapi.png') no-repeat;
+  background-size: 3em;
+}
+
+.search {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+span {
+  display: block;
+  flex-grow: 1;
+  text-transform: uppercase;
+  font-size: 0.6em;
+  letter-spacing: 1px;
+  font-weight: 700;
+  text-align: center;
+}
+
+.search input {
+  flex-grow: 2;
+  padding: 10px 6px;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  color: #555;
+}
+</style>
